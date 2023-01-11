867241001761c4122e6730a4298b8b345877641b
Sara Al-tayeh <saraaltayeh1@gmail.com>
2023-01-11 03:48:50 +0200

    rating

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Rating from "./Rating";
 
 function Product(props){
 const {product} = props;
@@ -13,9 +14,10 @@ return(
               <Link to={`/product/${product.slug}`}>
                 <Card.Title>{product.name}</Card.Title>
               </Link>
+              <Rating rating={product.rating} numReviews={product.numReviews}/>
               <Card.Text>${product.price}
               </Card.Text>
-              <Button>Add To Cart</Button>
+              <Button className="btn btn-warning">Add To Cart</Button>
             </Card.Body>
           </Card>
 )
