6b5291813aa9ca1c617efffff91d239c28f36f62
Sara Al-tayeh <saraaltayeh1@gmail.com>
2023-01-11 02:23:56 +0200

    responsive and add product component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,11 @@
-import { Link } from "react-router-dom";
+
 // import data from "../data";
 import axios from "axios";
 import { useEffect, useReducer } from "react";
-import logger from 'use-reducer-logger';
+import logger from "use-reducer-logger";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../Components/Product";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -22,18 +25,18 @@ function HomeScreen() {
     loading: true,
     error: "",
   });
-//   const [products, setProducts] = useState([]);
+  //   const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-        dispatch({type: "FETCH_REQUEST"});
-        try{
-            const result = await axios.get("/api/products");
-            dispatch({type: "FETCH_SUCCESS", payload: result.data});
-        } catch(err) {
-            dispatch({type: "FETCH_FAIL", payload: err.message});
-        }
-      
-    //   setProducts(result.data);
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+
+      //   setProducts(result.data);
     };
     fetchData();
   }, []);
@@ -42,28 +45,19 @@ function HomeScreen() {
     <div>
       <h1>List Products</h1>
       <div className="products">
-{
-    loading? (<div>loading ...</div>)
-    : error? (<div>{error}</div>)
-    : (
-        products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
-              <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-              </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add To Cart</button>
-            </div>
-          </div>
-        ))
-        )
-}
+        {loading ? (
+          <div>loading ...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
       </div>
     </div>
   );
